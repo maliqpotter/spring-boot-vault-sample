629c135a1f1687234a022e71c0c4c8a2aaa9aa74
hendisantika <hendisantika@yahoo.co.id>
2026-04-19 10:00:06 +0700

    feat: Init Project 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/persislabs/vault/SpringBootVaultSampleApplication.java
@@ -0,0 +1,13 @@
+package id.my.persislabs.vault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootVaultSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootVaultSampleApplication.class, args);
+    }
+
+}
